2797ddc4cae366db13fedb7a637aaa901d0cfeee
aesperer <piyrw9754@gmail.com>
2023-03-16 18:21:59 +0900

    :sparkles: OtpConfiguration

---
+++ src/main/kotlin/esperer/otp/config/OtpConfig.kt
@@ -0,0 +1,24 @@
+package esperer.otp.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+
+@Configuration
+@EnableWebSecurity
+class OtpConfig {
+
+    @Bean
+    fun passwordEncoder(): PasswordEncoder = BCryptPasswordEncoder()
+
+    fun configure(http: HttpSecurity) {
+        http.csrf()
+            .disable()
+        http.authorizeRequests()
+            .anyRequest()
+            .permitAll()
+    }
+}
